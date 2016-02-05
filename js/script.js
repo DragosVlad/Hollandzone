a0ecc1ec5c0bd30b54d11699d44a028565f400ea
DragosVlad <dragos.vlad95@gmail.com>
2016-02-05 08:42:58 -0800

    Activities page done

--- js/script.js
+++ js/script.js
@@ -64,11 +64,42 @@ $(document).ready(function(){
             $('.articles').after($('#sidebar--banner1'));
             $('.reports').after($('#sidebar--banner2'));
 
+
+            $('.january a').html('Jan');
+            $('.february a').html('Feb');
+            $('.march a').html('Mar');
+            $('.april a').html('Apr');
+            $('.may a').html('May');
+            $('.june a').html('Jun');
+            $('.july a').html('Jul');
+            $('.august a').html('Aug');
+            $('.september a').html('Sep');
+            $('.octomber a').html('Oct');
+            $('.november a').html('Nov');
+            $('.december a').html('Dec');
+
         } else {
           $('.main-content').after($('#sidebar'));
           $('.sidebar--box__recent').after($('#sidebar--banner1'));
           $('.sidebar--box__calendar').after($('#sidebar--banner2'));
+
+
+          $('.january a').html('January');
+          $('.february a').html('February');
+          $('.march a').html('March');
+          $('.april a').html('April');
+          $('.may a').html('May');
+          $('.june a').html('June');
+          $('.july a').html('July');
+          $('.august a').html('August');
+          $('.september a').html('September');
+          $('.octomber a').html('Octomber');
+          $('.november a').html('November');
+          $('.december a').html('December');
         };
+
+
+
     }; // End function checkSize()
 
 
